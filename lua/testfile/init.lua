7cf8da50d18d1776db331aa4c968d0286495da32
fj <freddy.a.jarva@gmail.com>
2023-10-21 20:23:24 +0200

    make test buf path configurable per file extension

--- lua/testfile/init.lua
+++ lua/testfile/init.lua
@@ -1,5 +1,28 @@
 local M = {}
 
+local _default_config = {
+    php = {
+        test_suffix = 'Test',
+        test_prefix = '',
+        test_dir = 'tests',
+        src_dir = 'src',
+    },
+    lua = {
+        test_suffix = '_spec',
+        test_prefix = '',
+        test_dir = 'tests',
+        src_dir = 'lua',
+    },
+}
+
+M._reset_config = function ()
+    M.config = _default_config
+end
+
+M.config = {
+    _default_config
+}
+
 M.create_test = function ()
     local current_file = vim.api.nvim_buf_get_name(0)
     if current_file == vim.fn.fnamemodify(current_file, ':.') then
@@ -8,16 +31,20 @@ M.create_test = function ()
     local extension = vim.fn.fnamemodify(current_file, ':e')
     local stub = vim.fn.fnamemodify(current_file, ':t:r')
 
-    local testfile_dir = vim.fn.fnamemodify(current_file, ':h:s?/src/?/tests/?')
+    local cfg = M.config[extension]
+    if cfg == nil then
+        print('did nothing: no config for file type ', extension)
+        return
+    end
+
+    local testfile_dir = vim.fn.fnamemodify(current_file, ':h:s?/'..cfg.src_dir..'/?/'..cfg.test_dir..'/?')
     if testfile_dir == vim.fn.fnamemodify(current_file, ':h') then
         print('did nothing: current file seems to be a test file')
         return
     end
 
-
     -- TODO make this change depending on extension/language
-    local testname_prefix = 'Test'
-    local testfile = testfile_dir..'/'..stub..testname_prefix..'.'..extension
+    local testfile = testfile_dir..'/'..cfg.test_prefix..stub..cfg.test_suffix..'.'..extension
 
     local testbuf = vim.fn.bufadd(testfile)
     vim.cmd.buffer(testbuf)
--- tests/tesfile_spec.lua
+++ tests/tesfile_spec.lua
@@ -1,20 +1,24 @@
 local spec_dir = '/tmp/testfile-spec'
 
 describe("testfile", function ()
+    before_each(function ()
+        vim.fn.mkdir(spec_dir)
+    end)
+
     after_each(function()
         if next(vim.fs.find('testfile-spec', {
             path = '/tmp'
         })) then
             vim.cmd('!rm -r '..spec_dir)
         end
+        require('testfile')._reset_config()
     end)
 
     it("can be required", function()
         require("testfile")
     end)
 
-    it("can create a new buffer with a testfile path from current buffer", function()
-        vim.fn.mkdir(spec_dir)
+    it("can create a new buffer with a testfile path from current buffer - php config", function()
         vim.fn.chdir(spec_dir)
         local bufnr = vim.fn.bufadd(spec_dir..'/src/Controller/LameController.php')
         vim.cmd.buffer(bufnr)
@@ -23,13 +27,33 @@ describe("testfile", function ()
         assert.are.same(spec_dir..'/tests/Controller/LameControllerTest.php', vim.api.nvim_buf_get_name(0))
     end)
 
+    it("can create a new buffer with a testfile path from current buffer - lua config", function()
+        vim.fn.chdir(spec_dir)
+        local bufnr = vim.fn.bufadd(spec_dir..'/lua/somename/awesome.lua')
+        vim.cmd.buffer(bufnr)
+        require("testfile").create_test()
+
+        assert.are.same(spec_dir..'/tests/somename/awesome_spec.lua', vim.api.nvim_buf_get_name(0))
+    end)
+
     it("does nothing if test dir translates to current buffers dir", function()
-        vim.fn.mkdir(spec_dir)
         vim.fn.chdir(spec_dir)
         local bufnr = vim.fn.bufadd(spec_dir..'/tests/Controller/LameController.php')
         vim.cmd.buffer(bufnr)
         require("testfile").create_test()
 
         assert.are.same(spec_dir..'/tests/Controller/LameController.php', vim.api.nvim_buf_get_name(0))
+    end)
+
+
+    it("does nothing if config doesn't exist for current buffers file extension", function()
+        local testfile = require('testfile')
+        testfile.config.php = nil
+        vim.fn.chdir(spec_dir)
+        local bufnr = vim.fn.bufadd(spec_dir..'/src/Controller/LameController.php')
+        vim.cmd.buffer(bufnr)
+        testfile.create_test()
+
+        assert.are.same(spec_dir..'/src/Controller/LameController.php', vim.api.nvim_buf_get_name(0))
     end)
 end)
