5b2142ed3ded28a3494beab9b72e031bcbe22d1c
fj <freddy.a.jarva@gmail.com>
2023-10-21 19:40:13 +0200

    add prefix to name of test buffer

--- lua/testfile/init.lua
+++ lua/testfile/init.lua
@@ -6,8 +6,12 @@ M.create_test = function ()
     if current_file == cwd_relative_file then
         print('current file must exist under cwd. aborting test creation.')
     end
-
-    local testfile = vim.fn.fnamemodify(current_file, ':s?/src/?/tests/?')
+    local extension = vim.fn.fnamemodify(current_file, ':e')
+    local stub = vim.fn.fnamemodify(current_file, ':t:r')
+    local testfile_dir = vim.fn.fnamemodify(current_file, ':h:s?/src/?/tests/?')
+    -- TODO make this change depending on extension/language
+    local testname_prefix = 'Test'
+    local testfile = testfile_dir..'/'..stub..testname_prefix..'.'..extension
 
     local testbuf = vim.fn.bufadd(testfile)
     vim.cmd.buffer(testbuf)
--- tests/tesfile_spec.lua
+++ tests/tesfile_spec.lua
@@ -1,20 +1,25 @@
-describe("tesfile", function ()
+local spec_dir = '/tmp/testfile-spec'
+
+describe("testfile", function ()
+    after_each(function()
+        if next(vim.fs.find('testfile-spec', {
+            path = '/tmp'
+        })) then
+            vim.cmd('!rm -r '..spec_dir)
+        end
+    end)
+
     it("can be required", function()
         require("testfile")
     end)
 
-    after_each(function()
-        vim.cmd('!rm -r /tmp/testfile-spec')
-    end)
-
     it("can create a new buffer with a testfile path from current buffer", function()
-        local spec_dir = '/tmp/testfile-spec'
         vim.fn.mkdir(spec_dir)
         vim.fn.chdir(spec_dir)
         local bufnr = vim.fn.bufadd(spec_dir..'/src/Controller/LameController.php')
         vim.cmd.buffer(bufnr)
         require("testfile").create_test()
 
-        assert.are.same(spec_dir..'/tests/Controller/LameController.php', vim.api.nvim_buf_get_name(0))
+        assert.are.same(spec_dir..'/tests/Controller/LameControllerTest.php', vim.api.nvim_buf_get_name(0))
     end)
 end)
